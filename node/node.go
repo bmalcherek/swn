071a3ed0b80046dbe08dd5221e9dddf8a51394ad
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2022-01-19 00:33:22 +0100

    improvements

--- node/node.go
+++ node/node.go
@@ -1,7 +1,7 @@
 package node
 
 import (
-	"fmt"
+	"log"
 	"math/rand"
 	"time"
 
@@ -9,8 +9,8 @@ import (
 )
 
 const (
-	timeout        = time.Second
-	ackFaultChance = 0.3
+	timeout        = 1500 * time.Millisecond
+	ackFaultChance = 0.5
 )
 
 type Node struct {
@@ -42,37 +42,41 @@ func (n *Node) Run() {
 func (n *Node) sendToken(id int) {
 	msg := message.Message{Type: message.Token, Id: id}
 	n.NextNodeSendChan <- msg
-	fmt.Printf("Node %d send token %v\n", n.NodeId, msg)
+	log.Printf("Node %d sent token %v\n", n.NodeId, msg)
 	n.lastSendId = id
 	go n.checkForRecievedAck()
 }
 
+func (n *Node) sendAck(id int) {
+	if rand.Float32() < ackFaultChance {
+		log.Printf("Node %d ack channel failed\n", n.NodeId)
+		time.Sleep(6 * time.Second)
+	}
+	msg := message.Message{Type: message.Ack, Id: id}
+	n.PrevNodeSendChan <- msg
+	log.Printf("Node %d sent ack %v\n", n.NodeId, msg)
+}
+
 func (n *Node) handlePrevNodeMessage(msg message.Message) {
-	fmt.Printf("Node %d recieved msg %v\n", n.NodeId, msg)
 	if n.lastRecievedTokenId == msg.Id {
 		return
 	}
+	log.Printf("Node %d recieved msg %v\n", n.NodeId, msg)
 	n.lastRecievedTokenId = msg.Id
-	if rand.Float32() < ackFaultChance {
-		fmt.Printf("Node %d ack channel failed\n", n.NodeId)
-		time.Sleep(6 * time.Second)
-	}
-	n.PrevNodeSendChan <- message.Message{Type: message.Ack, Id: msg.Id}
+	go n.sendAck(msg.Id)
 	time.Sleep(2 * time.Second)
 	n.sendToken(msg.Id + 1)
-	// n.NextNodeChan <- msg
-	// n.lastSendId = msg.Id
 }
 
 func (n *Node) handleNextNodeMessage(msg message.Message) {
-	fmt.Printf("Node %d recieved msg %v\n", n.NodeId, msg)
+	log.Printf("Node %d recieved msg %v\n", n.NodeId, msg)
 	n.lastRecievedAckId = msg.Id
 }
 
 func (n *Node) checkForRecievedAck() {
 	time.Sleep(timeout)
-	if n.lastSendId > n.lastRecievedAckId {
-		fmt.Printf("Node %d didn't recieve ack for %d, %v\n", n.NodeId, n.lastSendId, n.NextNodeSendChan)
+	if n.lastSendId > n.lastRecievedAckId && n.lastSendId > n.lastRecievedTokenId {
+		log.Printf("Node %d didn't recieve ack for %d, %v\n", n.NodeId, n.lastSendId, n.NextNodeSendChan)
 		n.sendToken(n.lastSendId)
 	}
 }
