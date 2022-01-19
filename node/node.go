965d596581b86908745eca5a652260c0a12cef44
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2022-01-19 11:45:09 +0100

    probably working recovery tokens

--- node/node.go
+++ node/node.go
@@ -9,20 +9,23 @@ import (
 )
 
 const (
-	timeout        = 1500 * time.Millisecond
-	ackFaultChance = 0.5
-	nodeCount      = 3
+	timeout              = 1500 * time.Millisecond
+	recvTokenFaultChance = 0.2
+	ackFaultChance       = 0.2
+	nodeCount            = 3
 )
 
 type Node struct {
-	NextNodeSendChan    chan<- message.Message
-	NextNodeRecvChan    <-chan message.Message
-	PrevNodeSendChan    chan<- message.Message
-	PrevNodeRecvChan    <-chan message.Message
-	NodeId              int
-	lastSendId          int
-	lastRecievedAckId   int
-	lastRecievedTokenId int
+	NextNodeSendChan          chan<- message.Message
+	NextNodeRecvChan          <-chan message.Message
+	PrevNodeSendChan          chan<- message.Message
+	PrevNodeRecvChan          <-chan message.Message
+	NodeId                    int
+	lastSentId                int
+	lastRecievedAckId         int
+	lastRecievedTokenId       int
+	lastSentRecoveryTokenId   int
+	lastRecievedRecoveryAckId int
 }
 
 func (n *Node) Run() {
@@ -33,9 +36,7 @@ func (n *Node) Run() {
 	for {
 		select {
 		case msg := <-n.PrevNodeRecvChan:
-			if rand.Float32() > 0.9999 {
-				n.handlePrevNodeMessage(msg)
-			}
+			n.handlePrevNodeMessage(msg)
 		case msg := <-n.NextNodeRecvChan:
 			n.handleNextNodeMessage(msg)
 		}
@@ -46,7 +47,7 @@ func (n *Node) sendToken(id int) {
 	msg := message.Message{Type: message.Token, Id: id}
 	n.NextNodeSendChan <- msg
 	log.Printf("Node %d sent token %v\n", n.NodeId, msg)
-	n.lastSendId = id
+	n.lastSentId = id
 	go n.checkForRecievedAck()
 }
 
@@ -60,10 +61,12 @@ func (n *Node) sendAck(id int) {
 	log.Printf("Node %d sent ack %v\n", n.NodeId, msg)
 }
 
-func (n *Node) sendRecoveryToken(id int) {
-	msg := message.Message{Type: message.RecoveryToken, Id: id, Target: (n.NodeId + 1) % nodeCount}
+func (n *Node) sendRecoveryToken(id, target int) {
+	msg := message.Message{Type: message.RecoveryToken, Id: id, Target: target}
 	n.PrevNodeSendChan <- msg
+	n.lastSentRecoveryTokenId = id
 	log.Printf("Node %d sent recovery token %v\n", n.NodeId, msg)
+	go n.checkForRecievedRecoveryAck(target)
 }
 
 func (n *Node) sendRecoveryAck(id int) {
@@ -73,15 +76,17 @@ func (n *Node) sendRecoveryAck(id int) {
 }
 
 func (n *Node) handlePrevNodeMessage(msg message.Message) {
-	if msg.Type == message.Token {
+	if msg.Type == message.Token && false {
 		if n.lastRecievedTokenId == msg.Id {
 			return
 		}
-		log.Printf("Node %d recieved msg %v\n", n.NodeId, msg)
+		log.Printf("Node %d recieved token %v\n", n.NodeId, msg)
 		n.lastRecievedTokenId = msg.Id
-		go n.sendAck(msg.Id)
-		time.Sleep(2 * time.Second)
-		n.sendToken(msg.Id + 1)
+		n.sendAck(msg.Id)
+		n.enterCriticalRegion(msg.Id)
+	} else if msg.Type == message.RecoveryAck {
+		log.Printf("Node %d recieved recovery ack %v\n", n.NodeId, msg)
+		n.lastRecievedRecoveryAckId = msg.Id
 	}
 }
 
@@ -91,14 +96,35 @@ func (n *Node) handleNextNodeMessage(msg message.Message) {
 		n.lastRecievedAckId = msg.Id
 	} else if msg.Type == message.RecoveryToken {
 		n.sendRecoveryAck(msg.Id)
+		if msg.Target != n.NodeId {
+			n.sendRecoveryToken(msg.Id, msg.Target)
+			return
+		}
+		n.enterCriticalRegion(msg.Id)
 	}
+}
+
+func (n *Node) enterCriticalRegion(id int) {
+	log.Printf("Node %d entered critical region\n", n.NodeId)
+	time.Sleep(2 * time.Second)
+	n.sendToken(id + 1)
 }
 
 func (n *Node) checkForRecievedAck() {
 	time.Sleep(timeout)
-	if n.lastSendId > n.lastRecievedAckId && n.lastSendId > n.lastRecievedTokenId {
-		log.Printf("Node %d didn't recieve ack for %d, %v\n", n.NodeId, n.lastSendId, n.NextNodeSendChan)
-		n.sendToken(n.lastSendId)
-		n.sendRecoveryToken(n.lastSendId)
+	if n.lastSentId > n.lastRecievedAckId && n.lastSentId > n.lastRecievedTokenId {
+		log.Printf("Node %d didn't recieve ack for %d\n", n.NodeId, n.lastSentId)
+		n.sendToken(n.lastSentId)
+		if n.lastSentId > n.lastRecievedRecoveryAckId {
+			n.sendRecoveryToken(n.lastSentId, (n.NodeId+1)%nodeCount)
+		}
+	}
+}
+
+func (n *Node) checkForRecievedRecoveryAck(target int) {
+	time.Sleep(timeout)
+	if n.lastSentRecoveryTokenId > n.lastRecievedRecoveryAckId {
+		log.Printf("Node %d didn't recieve recovery ack for %d, %v\n", n.NodeId, n.lastSentId, n.NextNodeSendChan)
+		n.sendRecoveryToken(n.lastSentRecoveryTokenId, target)
 	}
 }
